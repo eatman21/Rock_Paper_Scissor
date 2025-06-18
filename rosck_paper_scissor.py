6dac570ab89d944c9e900c3c7a70aba12b5f0006
Carol Pusey <caetman21@gmail.com>
2025-06-17 19:16:41 -0500

    Refactor game: fix indentation, add input validation, and improve structure

--- rosck_paper_scissor.py
+++ rosck_paper_scissor.py
@@ -1,50 +1,39 @@
 import random as ra
 
+emoji_map = {
+    'r': '🪨',
+    'p': '📃',
+    's': '✂️'
+}
+
 
 def game():
-    user = input("Enter a choice (r for rock, p for paper, s for scissor): ")
-    if user == "r":
-        user = "rock"
-    elif user == "p":
-        user = "paper"
-    elif user == "s":
-        user = "scissor"
-    computer = ra.choice(["rock", "paper", "scissor"])
-    if computer == "r":
-        computer = "rock"
-    elif computer == "p":
-        computer = "paper"
-    elif computer == "s":
-        computer = "scissor"
-    print(f"You chose {user}, computer chose {computer}")
+    choise = ('r', 'p', 's')
+    while True:  # loop to play again
+        print("Welcome to Rock, Paper, Scissor!")
+        user_choise = input('Rock, Paper, Scissor? (r/p/s): ').lower()
+        if user_choise not in choise:
+            print("Invalid choice. Please try again.")
+            continue
+        computer_choise = ra.choice(choise)
+        print(
+            f"You chose {emoji_map[user_choise]}, computer chose {emoji_map[computer_choise]}")
 
-    if user == computer:
-        return "It's a tie!"
-    elif user == "rock":
-        if computer == "scissor":
-            return "You win!"
-        else:
-            return "You lose!"
-    elif user == "paper":
-        if computer == "rock":
-            return "You win!"
-        else:
-            return "You lose!"
-    elif user == "scissor":
-        if computer == "paper":
-            return "You win!"
+        if user_choise == computer_choise:
+            print("It's a tie!")
+        elif (
+            (user_choise == 'r' and computer_choise == 's') or
+            (user_choise == 'p' and computer_choise == 'r') or
+                (user_choise == 's' and computer_choise == 'p')):
+            print("You win!")
         else:
             print("You lose!")
-            play_again = input("Do you want to play again? (y/n): ")
-            if play_again.lower() == 'y':
-                return game()
-            else:
-                return "Thanks for playing!"
+
+        play_again = input("Do you want to play again? (y/n): ").lower()
+        if play_again == 'n':
+            print("Thanks for playing!")
+            break
 
 
 if __name__ == "__main__":
-    while True:
-        result = game()
-        print(result)
-        if result == "Thanks for playing!":
-            break
+    game()
